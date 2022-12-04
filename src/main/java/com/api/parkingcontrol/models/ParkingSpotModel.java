4ccf390177f686b1cf529288a906f07116bd7dcc
Marcos Henrique <marcossistemas.ac@gmail.com>
2022-12-04 20:54:12 -0300

    create entity do bd e a classe com correção do nom

--- src/main/java/com/api/parkingcontrol/models/ParkingSpotModel.java
+++ src/main/java/com/api/parkingcontrol/models/ParkingSpotModel.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "TB_PARKING_SPOT")
-public class ParkingSoptModel implements Serializable{ 
+public class ParkingSpotModel implements Serializable{ 
     private static final long serialVersionUID = 1L; 
 
     

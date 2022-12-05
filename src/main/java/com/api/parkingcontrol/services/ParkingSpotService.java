13cbe4d98763b020c57d064a93d1741002987014
Marcos Henrique <marcossistemas.ac@gmail.com>
2022-12-04 23:04:05 -0300

    create service e injeção com construtor

---
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,18 @@
+package com.api.parkingcontrol.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+
+@Service
+public class ParkingSpotService {
+    
+    @Autowired
+    ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    } 
+
+}

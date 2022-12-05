53e7413778a2cb5b1b1f9235ba090d6eaf87fb3d
Marcos Henrique <marcossistemas.ac@gmail.com>
2022-12-04 23:04:38 -0300

    remoção do @Autowired e importação

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,6 +1,6 @@
 package com.api.parkingcontrol.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 
 import com.api.parkingcontrol.repositories.ParkingSpotRepository;
@@ -8,7 +8,7 @@ import com.api.parkingcontrol.repositories.ParkingSpotRepository;
 @Service
 public class ParkingSpotService {
     
-    @Autowired
+  
     ParkingSpotRepository parkingSpotRepository;
 
     public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
